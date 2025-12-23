8579f42ecf1f9cc21316be225c35945c99f42396
muhammadmulla1 <muhammad_mulla@icloud.com>
2025-12-23 15:29:31 +0000

    MTDSA-32425 - Return 504 GATEWAY_TIMEOUT for 499s and 504s from downstream take 2

--- test/shared/utils/ErrorHandlerSpec.scala
+++ test/shared/utils/ErrorHandlerSpec.scala
@@ -142,8 +142,8 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
       }
     }
 
-    "return 504 with error body" when {
-      Seq(499, 504).foreach { statusCode =>
+    "return GATEWAY_TIMEOUT with error body" when {
+      Seq(499, GATEWAY_TIMEOUT).foreach { statusCode =>
         s"a $statusCode UpstreamErrorResponse is returned" in new Test {
           val errorResponse: UpstreamErrorResponse = UpstreamErrorResponse("request timeout", statusCode, statusCode, Map.empty)
           val result: Future[Result]               = handler.onServerError(requestHeader, errorResponse)
